d2766b29bba1aaebbdf2f3d70139b7e90ece9291
Shehara Karunarathna <sheharakuni15@gmail.com>
2025-07-07 09:07:09 +0530

    files automatically created

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome to Shehara's Portfolio!</h1>
+        <button className ='first-button'> Say Hi </button>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
